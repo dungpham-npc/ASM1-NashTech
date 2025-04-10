c713592d11a3c704106443fc2aa83b08ff901f0d
Dung Pham <anhdungp0304@gmail.com>
2025-04-10 14:33:09 +0700

    Test commit to see if CI worked

---
+++ src/main/java/com/dungpham/asm1/service/impl/TestService.java
@@ -0,0 +1,10 @@
+package com.dungpham.asm1.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public int test(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/dungpham/asm1/TestServiceTest.java
@@ -0,0 +1,23 @@
+package com.dungpham.asm1;
+
+import com.dungpham.asm1.service.impl.TestService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestServiceTest {
+    private final TestService testService = new TestService();
+
+    @Test
+    void testAddition() {
+        // given
+        int a = 5;
+        int b = 7;
+
+        // when
+        int result = testService.test(a, b);
+
+        // then
+        assertEquals(12, result);
+    }
+}
